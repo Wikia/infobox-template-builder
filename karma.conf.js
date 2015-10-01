f556c857fb1d127da2835a2963081d32068173f6
Andrew Gleeson <andrewgleeson@Andrews-MacBook-Pro.local>
2015-10-01 14:10:15 -0700

    CONSF-32 | using karma-babel-preprocessor instead of karma-es6-transpiler-preprocessor

--- karma.conf.js
+++ karma.conf.js
@@ -23,7 +23,7 @@ module.exports = function(config) {
 		// preprocess matching files before serving them to the browser
 		// available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
 		preprocessors: {
-			'src/*.js': ['es6-transpiler']
+			'src/*.js': ['babel']
 		},
 
 		// test results reporter to use
