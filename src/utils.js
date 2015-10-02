b2dcbff7ae7f56008568fcf77a926e8f67069476
Kenneth Kouot <kenkouot@gmail.com>
2015-10-01 20:45:46 -0700

    fix typo

--- src/utils.js
+++ src/utils.js
@@ -4,23 +4,30 @@ export function deepSet(str, val, context = this) {
 		properties;
 
 	if (typeof str !== 'string') {
+
 		throw Error('value provided to the first argument of deepSet must be a string');
+
 	} else {
+
 		parts = str.split('.');
 	}
 
 	for (i = 0; i < parts.length; i++) {
 		// if a obj is passed in and loop is assigning last variable in namespace
 		if (i === parts.length - 1) {
+
 			Object.defineProperty(context, parts[i], {
 				configurable: true,
 				enumerable: true,
 				writable: true,
 				value: val
 			});
+
 			context = context[parts[i]];
+
 		} else {
-			// if namespace doesn't exist, icontexttantiate as empty object
+			
+			// if namespace doesn't exist, instantiate it as empty object
 			context = context[parts[i]] = context[parts[i]] || {};
 		}
 	}
