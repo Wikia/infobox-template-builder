fc51d826e3a8beb6906d84e1e6b0c1ac8c736de1
lizlux <liz@lizlux.net>
2015-10-05 11:19:10 -0700

    CONSF-40 initializing infobox theme class

--- src/serializers/xml.js
+++ src/serializers/xml.js
@@ -1,10 +1,12 @@
 'use strict';
+import {InfoboxThemeData} from '../InfoboxThemeData';
+
 export function serialize(data, theme) {
 }
 
 export function deserialize(doc) {
 	return {
 		data: null,
-		theme: null
+		theme: new InfoboxThemeData()
 	};
 }
