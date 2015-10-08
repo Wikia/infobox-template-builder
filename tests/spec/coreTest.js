ff8fe3a153c2f343f79a576641ffa365b21ef8fd
Andrew Gleeson <andrewgleeson@Andrews-MacBook-Pro.local>
2015-10-08 11:50:07 -0700

    CONSF-49 - added polyfill, now runs!

--- tests/spec/coreTest.js
+++ tests/spec/coreTest.js
@@ -1,5 +1,30 @@
 //require(['../../build']);
 
+if (!Function.prototype.bind) {
+  Function.prototype.bind = function (oThis) {
+    if (typeof this !== "function") {
+      // closest thing possible to the ECMAScript 5
+      // internal IsCallable function
+      throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+    }
+
+    var aArgs = Array.prototype.slice.call(arguments, 1),
+        fToBind = this,
+        fNOP = function () {},
+        fBound = function () {
+          return fToBind.apply(this instanceof fNOP && oThis
+                 ? this
+                 : oThis,
+                 aArgs.concat(Array.prototype.slice.call(arguments)));
+        };
+
+    fNOP.prototype = this.prototype;
+    fBound.prototype = new fNOP();
+
+    return fBound;
+  };
+}
+
 QUnit.test('Core Test', function(assert) {
 	// Setup the various states of the code you want to test and assert conditions.
 	console.log(new InfoboxTemplateBuilder());
