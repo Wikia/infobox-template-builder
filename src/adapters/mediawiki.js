5838db7c0fcdab1fb6ac8ddfa0e7e29ce8fa6862
lizlux <liz@lizlux.net>
2015-10-13 10:14:59 -0700

    CONSF-61 removing jQuery dependency

--- src/adapters/mediawiki.js
+++ src/adapters/mediawiki.js
@@ -1,6 +1,5 @@
 'use strict';
 
-import 'jquery';
 import {isString} from '../validators';
 
 /**
@@ -20,86 +19,78 @@ export function persist(infoboxTitle, xmlString) {
 		throw new TypeError('Infobox title and xml are required for saving to MediaWiki');
 	}
 
-	return $.when(getEditToken(infoboxTitle))
-		.done(save.bind(null, xmlString, infoboxTitle));
+	return new Promise(function (resolve, reject) {
+		getEditToken(infoboxTitle)
+			.then(save.bind(null, xmlString, infoboxTitle))
+	});
 }
 
 /**
- *
+ * Send request to MW API to save infobox article with new data
  * @param xmlString {string} New value for the infobox xml
  * @param infoboxTitle {string} Name of the article where the infobox xml will be saved
  * @param editToken {string} Needed for authenticating request
  * @return {jQuery} jQuery promise
  */
 function save(xmlString, infoboxTitle, editToken) {
-	var $deferred = $.Deferred();
-
-	$.ajax({
-		url: '/api.php',
-		data: {
+	return new Promise(function (resolve, reject) {
+		let xhr = new XMLHttpRequest();
+		let data = {
 			action: 'edit',
 			title: infoboxTitle,
 			text: xmlString,
 			token: editToken,
 			format: 'json'
-		},
-		dataType: 'json',
-		method: 'POST',
-		success: (resp) => {
-			if (resp && resp.edit && resp.edit.result === 'Success') {
-				$deferred.resolve();
-			} else if (resp && resp.error) {
-				$deferred.reject(resp.error.code);
-			} else {
-				$deferred.reject();
-			}
-		},
-		error: (err) => {
-			$deferred.reject(err);
-		}
-	});
+		};
 
-	return $deferred;
+		xhr.open('POST', '/api.php', true);
+		xhr.setRequestHeader('Content-Type', 'application/json');
+		xhr.send(JSON.stringify(data));
+		xhr.addEventListener('load', (response) => {
+			if (response && response.edit && response.edit.result === 'Success') {
+				resolve();
+			} else if (response && response.error) {
+				reject(response.error.code);
+			} else {
+				reject();
+			}
+		});
+	});
 }
 
 /**
- *
+ * Get an edit token so we can save an article via MW API
  * @param infoboxTitle {string} Name of the article where the infobox xml will be saved
  * @return {jQuery} jQuery promise
  */
 function getEditToken(infoboxTitle) {
-	var $deferred = $.Deferred();
-
-	$.ajax({
-		url: '/api.php',
-		method: 'POST',
-		data: {
+	return new Promise(function (resolve, reject) {
+		let xhr = new XMLHttpRequest();
+		let data = {
 			action: 'query',
 			prop: 'info',
 			titles: infoboxTitle,
 			intoken: 'edit',
 			format: 'json'
-		},
-		dataType: 'json',
-		success: (resp) => {
+		};
+
+		xhr.open('POST', '/api.php', true);
+		xhr.setRequestHeader('Content-Type', 'application/json');
+		xhr.send(JSON.stringify(data));
+		xhr.addEventListener('load', (response) => {
 			var editToken,
-				pages = resp.query.pages;
+				pages = response.query.pages;
 
 			if (pages) {
 				// get edit token from MW API response
 				editToken = pages[Object.keys(pages)[0]].edittoken;
 				if (editToken === undefined) {
-					$deferred.reject('noedit');
+					reject('noedit');
 				}
-				$deferred.resolve(editToken);
+				resolve(editToken);
 			} else {
-				$deferred.reject();
+				reject();
 			}
-		},
-		error: (err) => {
-			$deferred.reject(err);
-		}
+		});
 	});
-
-	return $deferred;
 }
