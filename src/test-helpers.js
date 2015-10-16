beb77fcfb01b7e1f68fd664a1e2797662c3233b0
lizlux <liz@lizlux.net>
2015-10-16 15:56:43 -0700

    CONSF-69 adding attribution

--- src/test-helpers.js
+++ src/test-helpers.js
@@ -1,3 +1,13 @@
+/*
+ * DOMParser HTML extension
+ * 2012-09-04
+ *
+ * By Eli Grey, http://eligrey.com
+ * Public domain.
+ * NO WARRANTY EXPRESSED OR IMPLIED. USE AT YOUR OWN RISK.
+ *
+ * @source https://gist.github.com/1129031
+ */
 (function(DOMParser) {
 	"use strict";
 
