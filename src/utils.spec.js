79d31c30a2c22058f1ac21e25df4fa32bad363d2
lizlux <liz@lizlux.net>
2015-10-19 13:57:07 -0700

    CONSF-67 adding simple test for xhrPost

--- src/utils.spec.js
+++ src/utils.spec.js
@@ -1,10 +1,10 @@
-import {deepSet, copyArray, swapArrayElements, serializeRequestData} from './utils';
+import {deepSet, copyArray, swapArrayElements, serializeRequestData, xhrPost} from './utils';
 
 'use strict';
 
 QUnit.module('utils');
 
-QUnit.test('deep set', function (assert) {
+QUnit.test('deep set', (assert) => {
 	deepSet('foo', 'bar');
 	assert.strictEqual(foo, 'bar', 'simple value should be set');
 
@@ -23,23 +23,29 @@ QUnit.test('deep set', function (assert) {
 	assert.strictEqual(obj.foo.bar, 1, 'deep property should be overridden');
 });
 
-QUnit.test('copy array', function (assert) {
-	let arr = [1,2];
+QUnit.test('copy array', (assert) => {
+	let arr = [1, 2];
 	let copy = copyArray(arr);
 	assert.deepEqual(arr, copy, 'array values should be copied to new array');
 });
 
-QUnit.test('swap array elements', function (assert) {
-	let arr = [1,2,3];
+QUnit.test('swap array elements', (assert) => {
+	let arr = [1, 2, 3];
 	swapArrayElements(arr, 0, 1);
-	assert.deepEqual(arr, [2,1,3], 'array elements should have changed places');
+	assert.deepEqual(arr, [2, 1, 3], 'array elements should have changed places');
 });
 
-QUnit.test('serialize request data', function (assert) {
+QUnit.test('serialize request data', (assert) => {
 	let data = {
 		foo: 'bar',
 		'baz?': 'qux&'
 	};
 	let string = serializeRequestData(data);
 	assert.strictEqual(string, 'foo=bar&baz%3F=qux%26', 'data should be serialized and encoded');
+});
+
+QUnit.test('xhr helper function', (assert) => {
+	assert.throws(() => {
+		xhrPost(1)
+	}, TypeError, 'xhrPost must be passed a string as the first argument');
 });
