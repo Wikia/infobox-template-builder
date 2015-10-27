cb5aea08655a2430099e3b81d9e84407d348819d
Andrew Gleeson <andrewgleeson@Andrews-MacBook-Pro.local>
2015-10-26 17:31:34 -0700

    CONSF-49 | updated persist test

--- src/adapters/mediawiki.spec.js
+++ src/adapters/mediawiki.spec.js
@@ -10,26 +10,4 @@ QUnit.test('MediaWiki persist()', function(assert) {
 	assert.throws( () => {mediawiki.persist('foo', {title: 'bar'})}, TypeError, 'Need both title and host options');
 	assert.throws( () => {mediawiki.persist('foo', {host: 'bar'})}, TypeError, 'Need both title and host options');
 	assert.throws( () => {mediawiki.persist(7, {title: 'bar', host: 'baz'})}, TypeError, 'XML must be a string');
-
-	let xmlString = '<infobox></infobox>',
-		options = {host: "http://lizlux.liz.wikia-dev.com", title: "Template:foobox"},
-		expected = {
-			edit: {
-				nochange: '',
-				pageid: 2718,
-				result: 'Success',
-				title: 'Template:Foobox'
-			}
-		};
-
-
-	let promise = mediawiki.persist(xmlString, options);
-	QUnit.stop();
-
-	promise.then( (actual) => {
-		QUnit.start();
-		assert.deepEqual(acutal, expected, 'Correct response');
-	}).catch( (err) => {
-		console.log(err);
-	});
 });
